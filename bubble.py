c546496c99f90712eed5a19bde6b62811ae15698
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2023-01-09 22:59:09 +0000

    Added bubble sort

---
+++ bubble.py
@@ -0,0 +1,11 @@
+def sort(arr: list[int]):
+    length = len(arr)
+
+    for i in range(length):
+        for j in range(0, length - i - 1):
+            if(arr[j] > arr[j + 1]):
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+print(sort([23,62,90,6,1]))
--- selection.py
+++ selection.py
@@ -1,11 +1,14 @@
 def sort(arr):
     for i in range(len(arr) - 1):
-        for j in range(i+1, len(arr)):
-            if(arr[j] < arr[i]):
-                tmp = arr[i]
-                arr[i] = arr[j]
-                arr[j] = tmp
+        minimum_pos = i
 
+        for j in range(i + 1, len(arr)):
+            if(arr[j] < arr[minimum_pos]):
+                minimum_pos = j
+
+        arr[i], arr[minimum_pos] = arr[minimum_pos], arr[i]
+    
     return arr
+
 
 print(sort([23, 14, 5, 3, 26, 2]))
